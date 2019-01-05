cf42b25176bf065af3db3b0f10aa2f2f14e6bab1
Jakub Kwiatkowski <jakkwi2@st.amu.edu.pl>
2019-01-05 16:42:51 +0100

    2019.01.05 microshell_ls added

--- microshell.c
+++ microshell.c
@@ -7,17 +7,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
-#include <wait.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <dirent.h>
 #include <error.h>
 #include <errno.h>
 
+#define COLOR_FILE "\x1B[37m"
 #define COLOR_FOLDER "\x1B[1;34m"
+#define COLOR_LINK "\x1B[36m"
+#define COLOR_DEVICE "\x1B[33m"
 #define COLOR_USER "\x1B[1;92m"
 #define COLOR_RESET "\x1B[0m"
 #define COLOR_HIDDEN "\x1B[1;90m"
 #define COLOR_IMPORTANT "\x1B[1;35m"
+#define FONT_BOLD "\x1B[1m"
 
-#define CLEAR_SCREEN "\x1B[2;J"
+#define CLEAR_SCREEN "\x1B[0;0H\x1B[2;J"
 
 #ifndef LOGIN_NAME_MAX
 #define LOGIN_NAME_MAX 65
@@ -87,18 +97,22 @@ int microshell_exit(char *args[]);
 
 int microshell_help(char *args[]);
 
+int microshell_ls(char *args[]);
+
 string built_in_names[] = 
 {
 	"cd",
 	"help",
-	"exit"
+	"exit",
+	"ls"
 };
 
 int (*built_in[])(char *args[]) = 
 {
 	&microshell_cd,
 	&microshell_help,
-	&microshell_exit
+	&microshell_exit,
+	&microshell_ls
 };
 
 int built_in_num()
@@ -497,19 +511,34 @@ string __trimaround(const string _source)
 int microshell_cd(char *args[])
 {
 	int status = 0;
+	string path = calloc(PATH_MAX + 1, sizeof(char));
+	char *sub = NULL;
 
 	if(!args[1])
 	{
 		printf("Error: no arguments for cd");
 		return _FAIL;
 	}
-	status = chdir(args[1]);
+	if((sub = strstr(args[1], "~/")) != NULL)
+	{
+		sub += 2;
+		path = strcat(path, getenv("HOME"));
+		path = strcat(path, "/");
+		path = strcat(path, sub);
+		printf("%s\n", path);
+	}
+	else
+	{
+		path = strcpy(path, args[1]);
+	}
+	status = chdir(path);
 	if(status == -1)
 	{
 		perror("Error");
 		errno = 0;
 		return _FAIL;
 	}
+	free(path);
 	return _SUCCESS;
 }
 
@@ -522,8 +551,207 @@ int microshell_exit(char *args[])
 int microshell_help(char *args[])
 {
 	printf(CLEAR_SCREEN);
-	printf("\x1B[90m" "\t\t\t\tMicroshell (2018)\n" COLOR_RESET);
-	printf(COLOR_IMPORTANT "Author: " COLOR_RESET "Jakub Kwiatkowski\n" COLOR_RESET);
-	
+	printf("\x1B[29G");
+	printf(COLOR_IMPORTANT "Microshell (2018/2019)\n" COLOR_RESET);
+	printf(COLOR_HIDDEN "Author: " COLOR_RESET "Jakub Kwiatkowski" "\x1B[61G" COLOR_HIDDEN "version: " COLOR_RESET "2019.01.05\n");
+	printf("This Microshell is simple Linux/Unix shell. \nIt allows you to execute programs right here in command line.\n");
+	printf("Arguments can be quoted in" FONT_BOLD " \"\" or \'\'" COLOR_RESET ". Type " FONT_BOLD "exit" COLOR_RESET " to exit microshell.\nEnjoy your new supertool!\n");
+
+	return _SUCCESS;
+}
+
+void microshell_ls_help()
+{
+	printf(CLEAR_SCREEN);
+	printf(FONT_BOLD "Microshell ls help.\n" COLOR_RESET);
+	printf(COLOR_IMPORTANT "Command:\n" COLOR_RESET "\tls [path] [options]\n");
+	printf(FONT_BOLD "Options:\n" COLOR_RESET);
+	printf("\t-a | display all content (including files started with .)\n");
+	printf("\t-l | display long info (user | group | size | modification date | name)\n");
+	printf("\t-q | display names in double quotes\n");
+	printf("\t-h | display this help\n");
+}
+
+string __getnamefromUID(uid_t UID)
+{
+	struct passwd *pwd = getpwuid(UID);
+	if(!pwd)
+	{
+		perror("User not found");
+		return NULL;
+	}
+	string _out = pwd->pw_name;
+	return _out;
+}
+
+string __getnamefromGID(gid_t GID)
+{
+	struct group *grp = getgrgid(GID);
+	if(!grp)
+	{
+		perror("Group not found");
+		return NULL;
+	}
+	string _out = grp->gr_name;
+	return _out;
+}
+
+int microshell_ls(char *args[])
+{
+	const char _delimeters[] = {' ', '\t', '\r', '\n', '\v', '\f'};
+
+	string path, filepath = calloc(PATH_MAX + 1, sizeof(char));
+	string COLOR  = COLOR_FILE;
+	string format = calloc(256, sizeof(char));
+	struct tm *mod_date;
+	DIR *directory;
+	struct dirent *entry;
+	struct stat entry_stat;
+	bool whitespace_name = false;
+
+	struct _disp
+	{
+		bool l;
+		bool a;
+		bool q;
+	}display = {false, false, false};
+
+	struct _props
+	{
+		string name;
+		string user;
+		string group;
+		char modtime[24];
+		off_t size;
+
+	}fprops;
+
+	string _args[] =
+	{
+		"-l",
+		"-a",
+		"-q",
+		"-h"
+	};
+
+	int i = 0, j = 0;
+	while(args[i] != NULL)
+	{
+		for(j = 0; j < (sizeof(_args)/sizeof(string)); j++)
+		{
+			if(!strcmp(args[i], _args[j]))
+			{
+				switch(j)
+				{
+					case 0:
+						display.l = true;
+						break;
+					case 1:
+						display.a = true;
+						break;
+					case 2:
+						display.q = true;
+						break;
+					case 3:
+						microshell_ls_help();
+						break;
+				}
+			}
+		}
+		i++;
+	}
+
+	path = args[1];
+	if(!path)
+		path = getcwd(NULL, 0);
+	if(!(directory = opendir(path)))
+	{
+		path = getcwd(NULL, 0);
+		if(!path)
+		{
+			perror("Cannot open current directory");
+			return _FAIL;
+		}
+		else if(!(directory = opendir(path)))
+		{
+			perror("Cannot open current directory");
+			return _FAIL;
+		}
+	}
+
+	while((entry = readdir(directory)) != NULL)
+	{
+		if((entry->d_name[0] == '.') && (!display.a))
+			continue;
+		for(i = 0; i < (sizeof(_delimeters)/sizeof(char)); i++)
+		{
+			if(strchr(entry->d_name, _delimeters[i]) != NULL)
+			{
+				whitespace_name = true;
+				break;
+			}
+			else
+				whitespace_name = false;
+		}
+		filepath = strcat(filepath, path);
+		filepath = strcat(filepath, "/");
+		filepath = strcat(filepath, entry->d_name);
+		if(stat(filepath, &entry_stat) == -1)
+		{
+			perror("Cannot read file properties");
+			printf("%s\n", entry->d_name);
+			continue;
+		}
+		
+		if(S_ISREG(entry_stat.st_mode))
+			COLOR = COLOR_FILE;
+		else if(S_ISDIR(entry_stat.st_mode))
+			COLOR = COLOR_FOLDER;
+		else if(S_ISLNK(entry_stat.st_mode))
+			COLOR = COLOR_LINK;
+		else if(S_ISBLK(entry_stat.st_mode))
+			COLOR = COLOR_DEVICE;
+
+		if(display.l)
+		{
+			format = strcat(format, "%s %s\t%d\t%.24s\t");
+			format = strcat(format, COLOR);
+			format = (display.q || whitespace_name) ? strcat(format, "\"%s\"\n") : strcat(format, "%s\n");
+			format = strcat(format, COLOR_RESET);
+
+			fprops.user = __getnamefromUID(entry_stat.st_uid);
+			fprops.group = __getnamefromGID(entry_stat.st_gid);
+			fprops.size = entry_stat.st_size;
+			mod_date = localtime(&entry_stat.st_mtim.tv_sec);
+			strftime(fprops.modtime, 24, "%a %Y/%m/%d %T", mod_date);
+			fprops.name = entry->d_name;
+
+			printf(format, fprops.user, fprops.group, fprops.size, fprops.modtime, fprops.name);
+		}
+		else
+		{
+			format = strcat(format, COLOR);
+			format = (display.q || whitespace_name) ? strcat(format, "\"%s\"\n") : strcat(format, "%s\n");
+			format = strcat(format, COLOR_RESET);
+
+			printf(format, entry->d_name);
+		}
+
+		memset(format, 0, 256);
+		memset(filepath, 0, PATH_MAX + 1);
+	}
+
+	free(format);
+	free(filepath);
+	return _SUCCESS;
+}
+
+int microshell_touch_help()
+{
+	return _SUCCESS;
+}
+
+int microshell_touch(char *args[])
+{
 	return _SUCCESS;
 }
